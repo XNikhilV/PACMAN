626a8954a2eceaddcaa5cb246d63f03de5304eb6
Nikhil <nikhilsai.vepuri12@gmail.com>
2025-02-23 00:31:18 -0500

    tilemap commit

--- src/PacMan.java
+++ src/PacMan.java
@@ -118,7 +118,36 @@ public class PacMan extends JPanel{
 
                 int x = c*tileSize;
                 int y = r*tileSize;
-                
+
+                if (tileMapChar == 'X'){ //block wall
+                    Block wall = new Block(wallImage, x , y, tileSize, tileSize);
+                walls.add(wall);
+                            }
+                else if (tileMapChar == 'b'){//blue ghost
+                    Block ghost = new Block(blueGhostImage, x, y, tileSize, tileSize)
+                    ghosts.add(ghost);    
+                }
+                else if (tileMapChar == 'o'){//orange ghost
+                    Block ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize)
+                    ghosts.add(ghost);    
+                }
+                else if (tileMapChar == 'p'){//pink ghost
+                    Block ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize)
+                    ghosts.add(ghost);    
+                }
+                else if (tileMapChar == 'r'){//red ghost
+                    Block ghost = new Block(redGhostImage, x, y, tileSize, tileSize)
+                    ghosts.add(ghost);    
+                }
+                else if (tileMapChar == 'P'){//Pacman
+                    pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+
+                    }
+                    else if (tileMapChar == ' '){//food
+                        Block food = new Block(null, x + 14, y + 14, 4, 4);
+                            
+                    }
+
             }
         }
 
