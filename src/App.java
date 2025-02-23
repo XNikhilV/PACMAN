ff11ed8c91492363e3a4e5fe956e7a042753f700
Nikhil <nikhilsai.vepuri12@gmail.com>
2025-02-23 00:52:12 -0500

    final

--- src/App.java
+++ src/App.java
@@ -1,29 +1,25 @@
 import javax.swing.JFrame;
 
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-       
-        int rowCount =21;
+        int rowCount = 21;
         int columnCount = 19;
-        int tileSize= 32;
-        int boardWidth= columnCount *tileSize;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
 
         JFrame frame = new JFrame("Pac Man");
-
-        //frame.setVisible(true); // sets the frame to visible
-        frame.setSize(boardWidth, boardHeight); // sets the size
-        frame.setLocationRelativeTo(null); // sets the location
-        frame.setResizable(false); // not resizable
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes on "X" button
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         PacMan pacmanGame = new PacMan();
         frame.add(pacmanGame);
         frame.pack();
+        pacmanGame.requestFocus();
         frame.setVisible(true);
-
 
     }
 }
